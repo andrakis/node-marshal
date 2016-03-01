73a3003a5e12a01755ec1fc29bb6605be17221c1
Julian Thatcher <julian.thatcher@freestyletechnology.com.au>
2016-03-02 10:52:13 +1100

    Make more modular

--- index.js
+++ index.js
@@ -1,30 +1,7 @@
-#!/usr/bin/env node
-
-var Worker = require('webworker-threads').Worker;
-var util = require('util');
 var server = require('./lib/server');
-var workerPath = __dirname + '/runtime/worker.js';
+var client = require('./lib/client');
+var shared = require('./lib/shared');
 
-// Start worker
-var worker = new Worker(workerPath);
-worker.onmessage = function(event) {
-	console.log("Received: " + util.inspect(event));
-	try {
-		var cmd = JSON.parse(event.data);
-		if(cmd.marshal) {
-			server.handleMarshalRequest(cmd.marshal);
-		}
-	} catch(e) {
-		console.log(e);
-		console.log(e.stack);
-	}
-};
-worker.onerror = function(event) {
-	console.log("Error", event);
-	console.log("Event stack:", event.stack);
-};
-
-server.Init();
-server.SetWorker(worker);
-console.log("Posting start");
-worker.postMessage("start");
+exports.Server = server;
+exports.Client = client;
+exports.Shared = shared;
